4576489ab67ed631df449d49e90aa2d39ddeeba7
Michael Walters <Michael1996Walters@gmail.com>
2023-05-15 14:12:46 +0100

    pushed handlebars main and Home

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const apiRoutes = require("./api");
+// const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
 
-router.use("/api", apiRoutes); //sends all routes starting with /api to the api router index
+router.use("/api", homeRoutes); //cahnge back to /api routes once complete
+//sends all routes starting with /api to the api router index
 router.use("/", homeRoutes); //sends other routes to homeroutes
 
 module.exports = router;
