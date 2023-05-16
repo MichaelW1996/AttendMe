a9e42df27b9391515a8ad6144f498ea16b281c24
Michael Walters <Michael1996Walters@gmail.com>
2023-05-16 18:40:46 +0100

    API calls for login made

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-// const { User } = require("../../models");
+const { User } = require("../../models");
 //currently only need guests for this, user routes may be usefull if required for organiser management- a user may want to remove another user?
 const { Guest } = require("../../models");
 
@@ -28,6 +28,45 @@ router.post("/new", async (req, res) => {
     res.status(200).json(newguest);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+//login
+
+router.post("/login", async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect username or password. Please try again!" });
+      return;
+    }
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect username or password. Please try again!" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
